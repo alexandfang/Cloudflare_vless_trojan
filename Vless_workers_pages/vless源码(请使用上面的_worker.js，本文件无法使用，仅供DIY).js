4c9ae33a67e1b247a5ae5731c2df809696b1e553
alexandfang <86596590+alexandfang@users.noreply.github.com>
2024-12-14 10:44:42 +0800

    Update vless源码(请使用上面的_worker.js，本文件无法使用，仅供DIY).js

--- Vless_workers_pages/vless源码(请使用上面的_worker.js，本文件无法使用，仅供DIY).js
+++ Vless_workers_pages/vless源码(请使用上面的_worker.js，本文件无法使用，仅供DIY).js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "e159d8c8-2406-426b-a42b-9eebdc2c0b4b";
 
 const proxyIPs = ["ts.hpc.tw"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
 const cn_hostnames = [''];
